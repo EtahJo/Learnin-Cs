7b64435d32a8330b2b134a8953f023cf6fb5d9b0
Etah <arrahetah23@gmail.com>
2023-06-01 02:30:22 +0100

    more learning

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -7,6 +7,9 @@ using System.Collections.Generic;
 
 namespace ConsoleApp2
 {
+
+    delegate double doubleIt(double val);
+
     public class Program
     {
 
@@ -14,6 +17,109 @@ namespace ConsoleApp2
 
         static void Main(string[] args)
         {
+            #region Lambda
+
+            //doubleIt dblIT = x => x * 2;
+
+            //Console.WriteLine($"5 * 2 = {dblIT(5)}");
+
+
+            #endregion
+            #region Where
+
+            //List<int> numList = new List<int> { 1, 9, 2, 6, 3 };
+
+            //var evenList = numList.Where(a => a % 2 == 0).ToList();
+
+            ////foreach (var j in evenList)
+            ////    Console.WriteLine(j);
+
+
+
+
+            // var rangeList = numList.Where(x => (x > 2) && (x < 9)).ToList();
+
+            //foreach (var j in rangeList)
+            //    Console.WriteLine(j);
+
+
+            //List<int> flipList = new List<int>();
+
+            //int i = 0;
+            //Random rnd = new Random();
+
+            //while (i < 100)
+            //{
+            //    flipList.Add(rnd.Next(1, 3));
+            //    i++;
+            //}
+
+            //Console.WriteLine("Heads: {0}",
+            //flipList.Where(a => a == 1).ToList().Count());
+
+
+            //Console.WriteLine("Tails: {0}",
+            //flipList.Where(a => a == 2).ToList().Count());
+
+            //var nameList = new List<string> { "Doug", "Sally", "Sue" };
+
+            //var sNames = nameList.Where(x => x.StartsWith("S"));
+
+            //foreach (var j in sNames)
+            //    Console.WriteLine(j);
+
+
+            #endregion
+
+            #region Select
+
+            //var OneTo10 = new List<int>();
+            //OneTo10.AddRange(Enumerable.Range(1, 10));
+
+            //var squares = OneTo10.Select(x => x * x);
+            // foreach(var j in squares)
+            //{
+            //    Console.WriteLine(j);
+            //}
+
+            #endregion
+
+            #region Zip, aggregrate, average,all,any,distinct,except,intersect
+
+            var numList2 = new List<int>() { 1,2,3,3,4,7,7,4,5};
+            //var listTwo = new List<int>(new int[] { 4, 5, 6 });
+
+            //var sumList = listOne.Zip(listTwo,(x,y)=>x+y).ToList();
+
+            //foreach(var i in sumList)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            Console.WriteLine("Sum {0}",
+
+                numList2.Aggregate((a, b) => a + b));
+
+
+            Console.WriteLine("Average {0}",
+
+             numList2.AsQueryable().Average());
+
+
+            Console.WriteLine("All > 3:{0}",
+
+            numList2.All(x=>x>3));
+
+
+            Console.WriteLine("Any Items >3: {0}",
+
+            numList2.Any(x=>x>3));
+
+            Console.WriteLine("Distint :{0}",
+                string.Join(", ", numList2.Distinct()));
+
+
+            #endregion
+
             #region ArrayList Code
 
             //ArrayList aList = new ArrayList();
@@ -139,37 +245,53 @@ namespace ConsoleApp2
 
             #region Generics
 
-            List<Animal> animalList = new List<Animal>();
+            //List<Animal> animalList = new List<Animal>();
 
-            List<int> numList = new List<int>();
+            //List<int> numList = new List<int>();
 
-            numList.Add(24);
+            //numList.Add(24);
 
-            animalList.Add(new Animal() { Name = "Doug" });
-            animalList.Add(new Animal() { Name = "Paul" });
-            animalList.Add(new Animal() { Name = "Sally" });
-            animalList.Insert(1, new Animal() { Name = "Steve" });
-            animalList.RemoveAt(1);
-            Console.WriteLine("Num of Animals: {0}",
-                animalList.Count
-                );
+            //animalList.Add(new Animal() { Name = "Doug" });
+            //animalList.Add(new Animal() { Name = "Paul" });
+            //animalList.Add(new Animal() { Name = "Sally" });
+            //animalList.Insert(1, new Animal() { Name = "Steve" });
+            //animalList.RemoveAt(1);
+            //Console.WriteLine("Num of Animals: {0}",
+            //    animalList.Count
+            //    );
 
-            foreach(Animal a in animalList)
-            {
-                Console.WriteLine(a.Name);
-            }
+            //foreach(Animal a in animalList)
+            //{
+            //    Console.WriteLine(a.Name);
+            //}
 
-            int x = 5, y = 4;
+            //int x = 5, y = 4;
 
-            Animal.GetSum<int>(ref x, ref y);
-            string strX = "5", strY = "4";
-            Animal.GetSum<string>(ref strX, ref strY);
+            //Animal.GetSum<int>(ref x, ref y);
+            //string strX = "5", strY = "4";
+            //Animal.GetSum<string>(ref strX, ref strY);
 
-            Rectangle<int> rec1 = new Rectangle<int>(20, 50);
-            Console.WriteLine(rec1.GetArea());
+            //Rectangle<int> rec1 = new Rectangle<int>(20, 50);
+            //Console.WriteLine(rec1.GetArea());
 
-            Rectangle<string> rec2 = new Rectangle<string>("20", "50");
-            Console.WriteLine(rec2.GetArea());
+            //Rectangle<string> rec2 = new Rectangle<string>("20", "50");
+            //Console.WriteLine(rec2.GetArea());
+            #endregion
+
+            #region delegates
+
+            //Arithmetic add, sub, addSub;
+            //add = new Arithmetic(Add);
+            //sub = new Arithmetic(Substract);
+
+            //addSub = add + sub;
+
+            //Console.WriteLine($"Add {6} & {10}");
+            //add(6, 10);
+            //Console.WriteLine($"Add & Substract {10} & {4}");
+
+            //addSub(10, 4);
+
             #endregion
         }
 
@@ -202,6 +324,19 @@ namespace ConsoleApp2
 
                 return string.Format($"{Width} * {dblLength}= {dblWidth * dblLength}");
             }
+        }
+
+        public delegate void Arithmetic(double num1, double num2);
+
+        public static void Add(double num1 , double num2)
+        {
+            Console.WriteLine($"{num1}+ {num2} = {num1 + num2}");
+
+        }
+        public static void Substract(double num1, double num2)
+        {
+            Console.WriteLine($"{num1}-{num2} = {num1 - num2}");
+
         }
     }
 }
